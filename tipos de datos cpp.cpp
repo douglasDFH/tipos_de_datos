d49435553a99f79dce1fae3fb7af2ca7bcaface0
douglasDFH <155786486+douglasDFH@users.noreply.github.com>
2024-01-11 21:46:27 -0400

    tipos de datos ejercicio en clases

---
+++ tipos de datos cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int entero = 15;
+	float flotante = 10.45;
+	double mayor = 16.3446;
+	char letra = 'a'
+	
+	cout<< flotante;
+	
+	
+	
+	
+	
+	return 0 ;
+}
